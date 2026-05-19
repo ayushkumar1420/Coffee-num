b291d333f6fa4e861656a3027de8c0a47e26f075
ayushkumar1420 <ayushmaurya496@gmail.com>
2026-05-19 23:56:30 +0530

    home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Home from './components/home'
 
 const App = () => {
   return (
   <>
     <Navbar />
+    <Home />
   </>
   )
 }
